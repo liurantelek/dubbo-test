458601c574cfb3f64b67692cde7d70fdecbc1647
liu-ran@smtp.telek.com.cn <454176801@qq.com>
2019-10-12 18:43:44 +0800

    web

---
+++ dubbo-user/user-provider/src/main/java/com/lr/dubbo/user/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.lr.dubbo.user;
+
+/**
+ * @author liuran
+ * @email: liu_ran_programmer@163.com
+ * @time 2019/10/12
+ * @Description: 描述:
+ */
+public class UserServiceImpl implements IUserService {
+}
